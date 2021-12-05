91e374d4afdb4757f1f20ce247abfd9f1f40b994
istruy <hello-ira@yandex.ru>
2021-12-05 12:37:56 +0300

    внесение правок

--- src/main.js
+++ src/main.js
@@ -7,7 +7,6 @@ import { createSortTemplate } from './view/sort-view.js';
 import { createDetailsTripTemplate } from './view/details-trip-view.js';
 import { renderTemplate, RenderPosition } from './render.js';
 import { getPoints } from './mock/mock.js';
-import { POINTS_COUNT } from './const.js';
 
 const siteMenuElement = document.querySelector('.trip-controls__navigation');
 const siteFilterElement = document.querySelector('.trip-controls__filters');
@@ -17,16 +16,16 @@ renderTemplate(siteFilterElement, RenderPosition.BEFOREEND, createFilterTemplate
 
 const sectionEventsElement = document.querySelector('.trip-events');
 
-const pointRoute = getPoints();
+const pointsRoute = getPoints();
 renderTemplate(sectionEventsElement, RenderPosition.BEFOREEND, createSortTemplate());
-renderTemplate(sectionEventsElement, RenderPosition.BEFOREEND, createFormEditingTemplate(pointRoute[0]));
+renderTemplate(sectionEventsElement, RenderPosition.BEFOREEND, createFormEditingTemplate(pointsRoute[0]));
 
-for (let i = 0; i < POINTS_COUNT - 1; i++) {
-  renderTemplate(sectionEventsElement, RenderPosition.BEFOREEND, createPointRouteTemplate(pointRoute[i]));
+for (let i = 0; i < pointsRoute.length - 1; i++) {
+  renderTemplate(sectionEventsElement, RenderPosition.BEFOREEND, createPointRouteTemplate(pointsRoute[i]));
 }
 
-renderTemplate(sectionEventsElement, RenderPosition.BEFOREEND, createFormCreationTemplate(pointRoute[9]));
+renderTemplate(sectionEventsElement, RenderPosition.BEFOREEND, createFormCreationTemplate(pointsRoute[9]));
 
 const detailsTripElement = document.querySelector('.trip-main');
-renderTemplate(detailsTripElement, RenderPosition.AFTERBEGIN, createDetailsTripTemplate());
+renderTemplate(detailsTripElement, RenderPosition.AFTERBEGIN, createDetailsTripTemplate(pointsRoute));
 
